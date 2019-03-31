cb9ffbbd70e3d1e2d19fd10b802033ed113c4d81
MickeyKen <mickey333ff@gmail.com>
2019-03-31 23:49:25 +0900

    [Add] transfomrf and listener

--- src/my_moving_server.cpp
+++ src/my_moving_server.cpp
@@ -6,6 +6,8 @@
 #include <math.h>
 #include <angles/angles.h>
 #include <nav_msgs/Odometry.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
 
 class WaypointMovingActionServer
 {
@@ -14,22 +16,30 @@ public:
   ~WaypointMovingActionServer(void) { };
   void goalCB();
   void preemptCB();
-  void executeCB(const my_robot_controller::WaypointMovingGoalConstPtr &goal);
+  void executeCB(const my_robot_controller::WaypointMovingGoalConstPtr &goal_);
 
 protected:
   ros::NodeHandle nh_;
   actionlib::SimpleActionServer<my_robot_controller::WaypointMovingAction> as_;
   std::string action_name_;
+  std::string base_frame;
+  std::string child_frame;
   bool start_;
   int number_of_waypoints, progress_;
   double dis_error_, theta_error_;
   double start_x_, start_y_, start_theta_;
   double angle_, len_;
+  double x_speed;
+  double y_speed;
+  double ang_speed;
+  int rate;
   my_robot_controller::WaypointMovingGoal goal_;
   my_robot_controller::WaypointMovingResult result_;
   my_robot_controller::WaypointMovingFeedback feedback_;
   geometry_msgs::Twist command_;
   ros::Publisher pub_;
+  tf::TransformListener listener;
+  tf::StampedTransform transform;
 };
 
 WaypointMovingActionServer::WaypointMovingActionServer(std::string name) :
@@ -40,8 +50,8 @@ WaypointMovingActionServer::WaypointMovingActionServer(std::string name) :
     &WaypointMovingActionServer::goalCB, this));
   as_.registerPreemptCallback(boost::bind(
     &WaypointMovingActionServer::preemptCB, this));
-  // as_.registerControlCallback(boost::bind(
-    // &WaypointMovingActionServer::controlCB, this));
+  // as_.registerExecuteCallback(boost::bind(
+  //   &WaypointMovingActionServer::executeCB, this));
   pub_ = nh_.advertise<geometry_msgs::Twist>(
     "/cmd_vel", 1);
     as_.start();
@@ -69,9 +79,73 @@ void WaypointMovingActionServer::executeCB(const my_robot_controller::WaypointMo
     return;
   if (progress_ < number_of_waypoints) {
 
-    double l_scale = 4.0;
-    double a_scale = 4.0;
-    double error_tol = 0.01;
+    geometry_msgs::Pose p = goal_.waypoint[progress_];
+    double target_x = p.position.x;
+    double target_y = p.position.y;
+
+    tf::Quaternion q(p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w);
+    tf::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    double target_theta = yaw;
+
+    double current_x = 0.0;
+    double current_y = 0.0;
+
+    rate = 20;
+    x_speed = 0.4;
+    y_speed = 0.4;
+
+    ros::Rate r(rate);
+
+    base_frame = "/base_footprint";
+    child_frame = "/odom";
+
+    if (target_x == target_y) {
+      if (target_x < 0) {
+        command_.linear.x = - (x_speed);
+      } else {
+        command_.linear.x = x_speed;
+      }
+      if (target_y < 0) {
+        command_.linear.y = - (y_speed);
+      } else {
+        command_.linear.y = y_speed;
+      }
+
+      while (current_x < target_x && current_y < target_y && nh_.ok()) {
+        pub_.publish(command_);
+        r.sleep();
+        try {
+          listener.waitForTransform(base_frame, child_frame,
+            ros::Time(0), ros::Duration(1.0));
+          listener.lookupTransform(base_frame, child_frame,
+            ros::Time(0), transform);
+        } catch (tf::TransformException &ex) {
+          ROS_ERROR("%s",ex.what());
+          ros::Duration(1.0).sleep();
+          continue;
+        }
+        current_x += fabs(transform.getOrigin().x());
+        current_y += fabs(transform.getOrigin().y());
+
+      }
+
+    } else if (target_x > target_y) {
+
+    } else {
+
+    }
+    command_.linear.x = 0;
+    command_.linear.y = 0;
+    pub_.publish(command_);
+    ROS_INFO("I'm getting to goal, %d/%d", progress_ + 1,
+    number_of_waypoints);
+    feedback_.progress = progress_;
+    as_.publishFeedback(feedback_);
+
+    start_ = true;
+    progress_++;
 
   } else {
     ROS_INFO("%s: Succeeded", action_name_.c_str());
