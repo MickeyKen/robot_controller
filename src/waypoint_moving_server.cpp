fd01b42985881329cf39541ab49c189b2d277293
MickeyKen <mickey333ff@gmail.com>
2019-03-31 12:23:14 +0900

    [Fix] complete waypoint_moving_server.cpp

--- src/waypoint_moving_server.cpp
+++ src/waypoint_moving_server.cpp
@@ -22,6 +22,7 @@ protected:
   std::string action_name_;
   bool start_;
   int number_of_waypoints, progress_;
+  double dis_error_, theta_error_;
   double start_x_, start_y_, start_theta_;
   double angle_, len_;
   my_robot_controller::WaypointMovingGoal goal_;
@@ -30,10 +31,10 @@ protected:
   geometry_msgs::Twist command_;
   ros::Subscriber sub_;
   ros::Publisher pub_;
-}
+};
 
-WaypointMovingActionServer::WaypointMovingActionServer(std::string name)
-: as_(nh_, name, false), action_name_(name)
+WaypointMovingActionServer::WaypointMovingActionServer(std::string name) :
+ as_(nh_, name, false), action_name_(name)
 {
   as_.registerGoalCallback(boost::bind(
     &WaypointMovingActionServer::goalCB, this));
@@ -41,7 +42,7 @@ WaypointMovingActionServer::WaypointMovingActionServer(std::string name)
     &WaypointMovingActionServer::preemptCB, this));
   sub_ = nh_.subscribe("/turtle1/pose", 1,
     &WaypointMovingActionServer::controlCB, this);
-  pub_ = nh_advertise<geometry_msgs::Twist>(
+  pub_ = nh_.advertise<geometry_msgs::Twist>(
     "/turtle1/cmd_vel", 1);
     as_.start();
 }
@@ -79,10 +80,47 @@ void WaypointMovingActionServer::controlCB(
       geometry_msgs::Pose p = goal_.waypoint[progress_];
       len_ = fabs(sqrt((p.position.x - start_x_)*(p.position.x - start_x_)
         + (p.position.y - start_y_) * (p.position.y - start_y_)));
-      angle = atan2((p.position.y - start_y_),(p.position.x - start_x_));
+      angle_ = atan2((p.position.y - start_y_),(p.position.x - start_x_));
     }
     ROS_DEBUG("current position:(%.3f, %.3f) theta: %.3f",
       msg->x, msg->y, msg->theta);
-    dis_error_ = 
+    dis_error_ = len_ - fabs(sqrt((start_x_ - msg->x) * (start_x_ - msg->x)
+    + (start_y_ - msg->y) * (start_y_ - msg->y)));
+    theta_error_ = angle_ - (msg->theta - start_theta_);
+
+    if (fabs(theta_error_) > error_tol) {
+      command_.linear.x = 0;
+      command_.angular.z = a_scale*theta_error_;
+    } else if (dis_error_ > error_tol) {
+      command_.linear.x = l_scale * dis_error_;
+      command_.angular.z = 0;
+    } else if (dis_error_ < error_tol && fabs(theta_error_ < error_tol)) {
+      command_.linear.x = 0;
+      command_.angular.z = 0;
+      ROS_INFO("I'm getting to goal, %d/%d", progress_ + 1,
+      number_of_waypoints);
+      feedback_.progress = progress_;
+      as_.publishFeedback(feedback_);
+
+      start_ = true;
+      progress_++;
+    } else {
+      command_.linear.x = l_scale * dis_error_;
+      command_.angular.z = a_scale * theta_error_;
+    }
+    pub_.publish(command_);
+  } else {
+    ROS_INFO("%s: Succeeded", action_name_.c_str());
+    result_.result = progress_;
+    as_.setSucceeded(result_);
   }
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "act_server");
+  WaypointMovingActionServer waypoint_moving(
+    ros::this_node::getName());
+  ros::spin();
+  return 0;
 }
